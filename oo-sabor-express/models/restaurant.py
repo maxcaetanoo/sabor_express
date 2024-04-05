c1b4978978b3466b7a7ff99e7eee8a4c5db15b0f
maxcaetanoo <maxsuelcaetano@live.com>
2024-04-05 11:25:23 -0300

    Updates restaurant.py object orientation classes and methods

--- oo-sabor-express/models/restaurant.py
+++ oo-sabor-express/models/restaurant.py
@@ -5,29 +5,48 @@ class Restaurant():
 
     # Função construtora do objeto restaurante
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
-        self.active = False
+        self._name = name.title()
+        self._category = category.title()#capitalize()
+        self._active = False
+
+        #Diferenças entre title e capitalize:
+        #title() = todas as primeiras letras maiusculas ex: Casa Grande
+        #capitalize() = apenas a primeira letra da primeira palavra maiuscula ex: Casa grande
 
         # Adcionando os objetos restaurante à nossa lista
         Restaurant.restaurant.append(self)
     
     # Função construtora de uma string do objeto
     def __str__(self):
-        return f'{self.name}, {self.category}, {"Ativo" if self.active else "Inativo"}'
+        return f'{self._name}, {self._category}, {"Ativo" if self.active else "Inativo"}'
     
     # Função para listar todos os restaurantes
-    def restaurant_list():
-        print("-"*53)
-        print(f"{'Nome':<10} | {'Categoria':^30} | {'Status'}")
-        print("-"*53)
+    @classmethod #Define o metodo que acessa a classe
+    def restaurant_list(cls):
+        print("-"*65)
+        print(f"|{'Nome'.ljust(20)} | {'Categoria'.ljust(30)} | {'Status'.ljust(7)}|")
+        print("-"*65)
         
-        for index in Restaurant.restaurant:
-            print(f'{index.name:<10} | {index.category:^30} | {'Ativo' if index.active else 'Inativo'}')
+        for index in cls.restaurant:
+            print(f'|{index._name.ljust(20)} | {index._category.ljust(30)} | {index.active:^7}|')
+        print("-"*65)
+    
+    #Metodo que define como será apresentada a propriedade 'active'
+    @property
+    def active(self):
+        return '☑' if self._active else '☐'
+    
+    #Metodo para alternar o status de um objeto
+    def alternate_status(self):
+        self._active = not self._active
         
+# #Objetos restaurantes sendo criados
+# restaurante_comedoria = Restaurant('Comedoria', 'Restaurante de massas')
+# restaurante_pan_pop = Restaurant('Pan Pop', 'Restaurante de sanduiches')
+# restaurante_sushi_sing = Restaurant('sushi sing', 'Restaurante de sushi')
 
-restaurante_comedoria = Restaurant('Comedoria', 'Comida italiana')
+# #Existem duas formar de usar o metodo alternate_status, diretamente através do objeto ou através da classe
+# restaurante_sushi_sing.alternate_status() #Objeto
+# Restaurant.alternate_status(restaurante_pan_pop) #Classe
 
-restaurante_pan_pop = Restaurant('Pan Pop', 'Restaurante de sanduiches')
-
-Restaurant.restaurant_list()
+# Restaurant.restaurant_list()
